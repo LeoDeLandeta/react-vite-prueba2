f117a1ee4a97caab270a2a7c85aea5df24c2f40d
leodelandeta <116042126+LeoDeLandeta@users.noreply.github.com>
2024-05-15 19:20:33 -0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+import logo from '../assets/images/logo.svg'
+import { NavBar } from './NavBar'
+
+export const Header = () => {
+  return (
+    <header className='flex place-content-between items-center mb-8'>
+        <img src={logo} alt="" />
+        <NavBar />
+    </header>
+  )
+}
+
+
